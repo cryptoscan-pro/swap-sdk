73882908f6c6be976220fb5d898cf28aab2a5e84
Daniil Jave <62588539+javeoff@users.noreply.github.com>
2024-08-12 01:17:01 +0400

    feat: upgrade types

--- lib/index.ts
+++ lib/index.ts
@@ -2,4 +2,6 @@ import { swap } from './utils/swap.js';
 export { swap } from './utils/swap.js';
 export { createTransaction } from './utils/createTransaction.js';
 
+export type { SwapParams } from './utils/swap.js';
+
 export default swap;
